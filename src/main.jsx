680ea882831e7c1edf166d9282b3fe1bf9baa21b
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-02-24 20:03:08 +0900

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { GlobalStyle } from "./styles/GlobalStyle.js";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <ThemeProvider theme={{ theme }}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </>
+);
---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+const fontFamily = '"Pretendard Variable" , "Noto Sans KR", sans-serif';
+
+const fontSizes = {
+  xs: "12px",
+  sm: "14px",
+  base: "16px",
+  lg: "18px",
+  xl: "22px",
+  xxl: "28px",
+};
