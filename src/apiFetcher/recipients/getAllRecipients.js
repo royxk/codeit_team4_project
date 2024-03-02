1652047cb5e4f54fc34f13c5fe77f68277f0a210
Sfer7 <forever_kaka@naver.com>
2024-03-02 11:17:08 +0900

    #fix : getAllRecipients 오류 수정

--- src/apiFetcher/recipients/getAllRecipients.js
+++ src/apiFetcher/recipients/getAllRecipients.js
@@ -3,7 +3,7 @@ import customAxios from "../customAxios.js";
 export const getAllRecipients = async (limit = 8, offset = 0) => {
   return await customAxios({
     method: "get",
-    url: "/recipients/?limit=${limit}&offset=${offset}",
+    url: `/recipients/?limit=${limit}&offset=${offset}`,
     headers: {
       "Content-Type": "application/json",
     },
