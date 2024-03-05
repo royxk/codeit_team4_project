a08838c001eb377410ac348eb332a08024a03077
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-03-05 22:50:41 +0900

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// aws-sdk 요놈이 node 를 건드리는  것 같습니다
+// 근데 vite가 client다 보니 node환경을 배제하고 빌드를 마는 것 같아요
+// 혹시 모르니까 잊지말고 vite.config.js에 아래와 같이 설정을 해주시면 될 것 같아요
+
 export default defineConfig({
   plugins: [react()],
   define: { _global: {} },
@@ -21,3 +25,5 @@ export default defineConfig({
     },
   },
 });
+
+//
