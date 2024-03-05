47c9743e6fd31a1300bb8674bbf5303054c1bd4a
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-03-05 21:40:46 +0900

    1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    resolve: {
+      alias: {
+        "./runtimeConfig": "./runtimeConfig.browser",
+      },
+    },
     global: {},
   },
 
