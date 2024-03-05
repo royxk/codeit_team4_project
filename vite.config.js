050833d69b947afea62d8883ea627ee6738f9357
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-03-05 21:54:17 +0900

    updatednodePolyFills

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import nodePolyfills from "rollup-plugin-polyfill-node";
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
@@ -16,5 +16,5 @@ export default defineConfig({
     sourcemap: true,
   },
 
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
 });
