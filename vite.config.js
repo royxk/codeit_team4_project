5cf9223a7473c1a0bbd2438a7e4ac39e9b89dea4
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-03-05 22:05:58 +0900

    pleasssssssss

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    resolve: {
-      alias: {
-        "./runtimeConfig": "./runtimeConfig.browser",
-      },
+  plugins: [react()],
+  define: { _global: {} },
+
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser", // 모듈의 실제 위치를 브라우저 번들러에게 알려줌
     },
-    global: {},
   },
 
   build: {
     sourcemap: true,
+    commonjsOptions: {
+      include: [/node_modules/], // 번들에 포함시킬 모듈의 경로
+      extensions: [".js", ".cjs"], // CommonJS 모듈로 간주할 파일의 확장자
+      strictRequires: true, // require 구문에 해당 모듈이 없을 경우 에러 발생
+      transformMixedEsModules: true, // import와 require문을 함께 사용하는 경우 이를 번들에 포함시키기 위함
+    },
   },
-
-  plugins: [react()],
 });
