bc296d707fd07f518830bf3185fd505126fc5fed
Sfer7 <forever_kaka@naver.com>
2024-03-06 11:21:40 +0900

    #fix : ChangeInnerHTMLSafely 기능 수정

---
+++ src/utils/ChangeInnerHTMLSafely.jsx
@@ -0,0 +1,9 @@
+import DOMPurify from "dompurify";
+import whitelist from "../common/whitelist.js";
+
+export const ChangeInnerHTMLSafely = (HTML) => {
+    const sanitizer = DOMPurify.sanitize(HTML, whitelist)
+    return (
+        <div dangerouslySetInnerHTML={{__html: sanitizer(HTML)}} />
+    )
+}
