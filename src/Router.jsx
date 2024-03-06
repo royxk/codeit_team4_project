ad2c4157e1e61255e8171c2a1cf957fed22c0b6b
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-03-06 21:18:36 +0900

    updated

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import List from "./pages/List";
 import Papers from "./pages/Papers";
-import PaperEdit from "./pages/PaperEdit";
 import MessageCreate from "./pages/MessageCreate.jsx";
 import PaperCreate from "./pages/PaperCreate";
 
@@ -13,7 +12,7 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<List />} />
         <Route path="/post/:id" element={<Papers />} />
-        <Route path="/post/:id/edit" element={<PaperEdit />} />
+        <Route path="/post/:id/edit" element={<Papers />} />
         <Route path="/post/:id/message" element={<MessageCreate />} />
         <Route path="/PaperCreate" element={<PaperCreate />} />
       </Routes>
