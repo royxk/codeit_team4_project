576ce7433a76c215a351f39ab6a72273b4e6c353
Sfer7 <forever_kaka@naver.com>
2024-03-07 23:52:13 +0900

    #fix

--- src/pages/MessageCreate.jsx
+++ src/pages/MessageCreate.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {useNavigate, useParams} from "react-router-dom";
 import NavBar from "../components/core/NavBar.jsx";
 import styled from "styled-components";
